a092ad6ef7fecc1c38539071ea26432e3e46226d
diss <65290988+Dissurender@users.noreply.github.com>
2023-10-02 08:54:06 -0500

    dockerfile added

---
+++ src/main/java/dev/diss/shelfish/repository/ReaderRepository.java
@@ -0,0 +1,7 @@
+package dev.diss.shelfish.repository;
+
+import dev.diss.shelfish.model.Reader;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReaderRepository extends JpaRepository<Reader, Long> {
+}
